39587522f592c643b847347879d461912067381a
Chamindu Henepola <96916532+hmcshenepola@users.noreply.github.com>
2022-10-18 00:03:23 +0530

    Create RegisterServlet.java

---
+++ src/java/RegisterServlet.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Dewmin
+ */
+public class RegisterServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet RegisterServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            
+            //fetch data from registration page
+            String name = request.getParameter("name");
+            String email = request.getParameter("email");
+            String password = request.getParameter("password");
+            
+            //make user object
+            User userModel = new User(name, email, password);
+            
+            //create a database model
+            UserDatabase regUser = new UserDatabase(ConnectionPro.getConnection());
+            if (regUser.saveUser(userModel)) {
+               response.sendRedirect("User.jsp");
+            } else {
+                String errorMessage = "User Available";
+                HttpSession regSession = request.getSession();
+                regSession.setAttribute("RegError", errorMessage);
+                response.sendRedirect("register.jsp");
+                }
+
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
