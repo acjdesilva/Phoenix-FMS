1235c3490abb2e7e4a52aca0b84f20810f4f7070
amcnkarandawala <99523707+amcnkarandawala@users.noreply.github.com>
2022-10-17 23:32:06 +0530

    create bookingtwo.java

---
+++ src/java/bookingtwo.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
