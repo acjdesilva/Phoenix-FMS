6ec33918b6783ab857e5308cfa6c3a47ee6ea7ed
amcnkarandawala <99523707+amcnkarandawala@users.noreply.github.com>
2022-10-17 23:38:35 +0530

    create staffgradeone.java

---
+++ src/java/staffgradeone.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
